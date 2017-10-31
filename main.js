2349f784aef6b1f7e7ff8f391abfa2ac21c9107a
nick <nicholasb1ake@hotmail.com>
2017-10-31 12:05:38 +0000

    Functions

---
+++ main.js
@@ -0,0 +1,61 @@
+// var result = 5 + 10;
+// var result2 = 10 + 10;
+// var result3 = 11 - 2;
+//
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+//
+// //this is a named function.
+// function addNumbers (){
+//   var result = 5 + 10;
+//   console.log(result);
+// }
+// //This is calling the function.
+// addNumbers();
+//
+// //This is an anon fucntion.
+// var anonAddNumbers = function() {
+//   var result = 5 + 10;
+//   console.log(result);
+// }
+//
+// function helloWorld () {
+//   console.log('Hello world')
+// }
+//
+// helloWorld();
+//
+// var helloWorld = function(){
+//   console.log('Hello World')
+// }
+//  helloWorld ();
+
+// makeNoise();
+//
+// function makeNoise(){
+//   alert('how annoying');
+// }
+
+// function addNumbers (number1, number2){
+//   var result = number1 + number2;
+//   console.log(result);
+// }
+// addNumbers(2,10);
+
+// function addNumbers (number1, number2){
+//    return number1 + number2
+//
+// }
+//
+// var answer1 = addNumbers (2,10);
+//
+// console.log(answer1);
+
+
+// function addNumbers (1, 3){
+//    return 1 + 3;
+//
+// }
+//
+// addNumbers (2,10);
